6f5f0d7047c98730af0f60fb5b5cf3703e6ca315
kkokwara2014 <kkokwara2014@gmail.com>
2019-12-06 06:20:23 +0100

    Installing image intervention

--- app/Bank.php
+++ app/Bank.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    //
+    
+
+    public function bankaccount(){
+        return $this->hasMany(Bankaccount::class);
+    }
 }
--- app/Bankaccount.php
+++ app/Bankaccount.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bankaccount extends Model
 {
-    //
+    
+    public function bank(){
+        return $this->belongsTo(Bank::class);
+    }
 }
